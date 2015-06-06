6bf2891da5636b990535ec74b9bdd7153a0863d3
Otavio Fernandes <oaugustus@gmail.com>
2015-06-06 14:15:20 -0300

    Fix

--- src/Singular/Register/ControllerRegister.php
+++ src/Singular/Register/ControllerRegister.php
@@ -154,12 +154,6 @@ class ControllerRegister
         if ($annotation->method != null || !empty($annotation->methods)) {
 
             $routeMethods = $annotation->method == null ? $annotation->methods : array($annotation->method);
-            
-            if (isset($app['cors.enabled'])) {
-                if ($app['cors.enabled']) {
-                    $routeMethods[] = 'options';
-                }
-            }
 
             if ($annotation->pattern) {
 
